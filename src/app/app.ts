abd3090039592e87b0c9ab042423c528cc8279b9
Ganesh <gborase81@gmail.com>
2025-06-29 02:27:12 +0530

    Added changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Profile } from './profile/profile';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login,Signup,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my-angular-app';
+  name='Ganesh';
+  x=10;
+  y=20;
+  user1='Ganesh';
+  user2='Shubham';
 }
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+//   templateUrl:'./profile.html',
+template:'<h1>Profile Page</h1>',
+})
+export class Profile {
+
+}
